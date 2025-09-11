142f6189e30129a071297ae92ee23812682654bb
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-09-11 13:05:32 +0530

    pom update

--- src/main/java/com/housecleaning/SparkleCleanBackendApplication.java
+++ src/main/java/com/housecleaning/SparkleCleanBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SparkleCleaningBackendApplication {
+public class SparkleCleanBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SparkleCleaningBackendApplication.class, args);
+        SpringApplication.run(SparkleCleanBackendApplication.class, args);
     }
 }
